2bd9e2e681a267bb411a54005963153682f720cd
Logan Hennen <logan.hennen@mail.wlc.edu>
2023-04-03 22:44:20 -0500

    heros added

--- content/actors/heros.h
+++ content/actors/heros.h
@@ -4,8 +4,9 @@
 #include "none.h"
 
 namespace Heros {
-    
-    constexpr int default_speed{8};
-    const HeroType nobody{"none", default_speed, 1, std::make_shared<None>(), {}};
-    
-}
+
+constexpr int default_speed{8};
+const HeroType nobody{
+    "creeper", default_speed, 1, std::make_shared<None>(), {}};
+
+}  // namespace Heros
