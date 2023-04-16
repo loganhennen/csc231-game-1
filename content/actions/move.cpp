d915b57466880319ba87cc473b93c274ce9ea6e0
Logan Hennen <logan.hennen@mail.wlc.edu>
2023-04-16 15:26:37 -0500

    add connal stuff

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -1,5 +1,9 @@
 #include "move.h"
 
 Result Move::perform(Engine& engine) {
+    Vec position = actor->get_position();
+    actor->move_to(position + direction);
     return success();
 }
+
+Move::Move(Vec direction) : direction{direction} {}
--- content/actors/heros.h
+++ content/actors/heros.h
@@ -8,4 +8,14 @@ namespace Heros {
 constexpr int default_speed{8};
 const HeroType nobody{"dragon", default_speed, 1, std::make_shared<None>(), {}};
 
+// namespace Heros
+
+// Reaction = std::function<std::unique_ptr<Action>()>
+// std:: unique_ptr do_stuff();
+const std::unordered_map<std::string, Reaction> keybindings = {
+    {"A", []() {
+         return std::make_unique<Move>(Vec{-1, 0});
+     }}};  // namespace Heros
+const HeroType nobody{"shy-guy", default_speed, 1, std::make_shared<None>(),
+                      keybindings};
 }  // namespace Heros
