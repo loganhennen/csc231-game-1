a6483f45286487a0fa323603aa8ffe572a9963a5
Logan Hennen <logan.hennen@mail.wlc.edu>
2023-04-16 15:37:57 -0500

    more connal changes

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -1,9 +1,13 @@
 #include "move.h"
 
+#include <iostream>
+
+#include "actor.h"
+
+Move::Move(Vec direction) : direction{direction} {}
+
 Result Move::perform(Engine& engine) {
     Vec position = actor->get_position();
     actor->move_to(position + direction);
     return success();
 }
-
-Move::Move(Vec direction) : direction{direction} {}
--- content/actors/heros.h
+++ content/actors/heros.h
@@ -1,15 +1,10 @@
 #pragma once
 
 #include "herotype.h"
+#include "move.h"
 #include "none.h"
 
 namespace Heros {
-
-constexpr int default_speed{8};
-const HeroType nobody{"dragon", default_speed, 1, std::make_shared<None>(), {}};
-
-// namespace Heros
-
 // Reaction = std::function<std::unique_ptr<Action>()>
 // std:: unique_ptr do_stuff();
 const std::unordered_map<std::string, Reaction> keybindings = {
@@ -17,5 +12,9 @@ const std::unordered_map<std::string, Reaction> keybindings = {
          return std::make_unique<Move>(Vec{-1, 0});
      }}};  // namespace Heros
 const HeroType nobody{"shy-guy", default_speed, 1, std::make_shared<None>(),
+                      keybindings};
+
+constexpr int default_speed{8};
+const HeroType nobody{"dragon", default_speed, 1, std::make_shared<None>(),
                       keybindings};
 }  // namespace Heros
